539d9fdbeb29123d2097169881c5e73aa904f1d2
JKJK <jackie.shuy@gmail.com>
2020-01-04 12:54:53 +1300

    create api folder and get all code router on server side

--- server/routes/codes.js
+++ server/routes/codes.js
@@ -2,4 +2,16 @@ const express = require ('express')
 const db = require('../db/db')
 const router = express.Router()
 
-router.get('')
+module.exports = router
+
+const sendGenericErrorMessage = (res) => {
+    res.status(500).send(
+      "An Unexpected error has occurred and we're looking into it"
+    )
+  }
+
+router.get('/codes', (req,res) => {
+    return db.getAllCode()
+        .then(codes => res.json(codes))
+        .catch(()=> sendGenericErrorMessage(res))
+})
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
+const codes = require('./routes/codes')
 
 const server = express()
 
 server.use(express.json())
+server.use('/api/v1', codes)
 server.use(express.static(path.join(__dirname, './public')))
 
 module.exports = server
