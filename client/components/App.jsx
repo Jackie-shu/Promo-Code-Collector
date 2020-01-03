39dad265514fddb50c3d5d08ac220ac24d7ad611
JKJK <jackie.shuy@gmail.com>
2020-01-04 12:46:51 +1300

    make app component working without Router tag

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,21 +4,23 @@ import Nav from './Nav'
 import Footer from './Footer'
 import Adv from './Adv'
 import AllCode from './AllCode'
-import { Route, Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Router>
+      <div>
+        {/* <Router> */}
           <Route path='/' component={Header} />
           <Route path='/' component={Nav} />
           <Route exact path='/' component={Adv} />
-          <br />
-          <Route path='/codes' component={AllCode} />
+          <Switch>
+            <Route exact path='/codes' component={AllCode} />
+            
+          </Switch>
           <Route path='/' component={Footer} />
-        </Router>
-      </>
+        {/* </Router> */}
+      </div>
     )
   }
 }
