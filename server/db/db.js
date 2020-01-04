6cdf1a46bbc88bb5f2d81b4ab841a61b1715202b
JKJK <jackie.shuy@gmail.com>
2020-01-04 23:43:37 +1300

    change db function to get code with website detail

--- server/db/db.js
+++ server/db/db.js
@@ -7,8 +7,7 @@ module.exports = {
 
 function getAllCode(db = connection) {
     return db('code')
+        .join('website','code.website_id','website.id')
+        .select('code.id as id', 'website.id as websiteId', 'code.code as code', 'website.name as name', 'website.website as website')
 }
 
-function getAllWebsite(db = connection) {
-    return db('website')
-}
